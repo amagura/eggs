f18d422136785cfb8716b45a0667e1cfe7dc781b
Alexej Magura <amagura@users.noreply.github.com>
2015-01-17 21:17:47 -0700

    Update readline-egg.c

--- readline/trunk/readline-egg.c
+++ readline/trunk/readline-egg.c
@@ -3,7 +3,7 @@
 //
 // Copyright (c) 2002 Tony Garnock-Jones
 // Copyright (c) 2006 Heath Johns (paren bouncing and auto-completion code)
-// Copyright (c) 2015 Alexej Magura (added a lot of history functions and more)
+// Copyright (c) 2014 Alexej Magura (added a lot of history functions and more)
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
